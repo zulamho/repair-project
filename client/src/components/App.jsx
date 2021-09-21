b13c2c3a13dac90cf80f04a0c691523e49819cc2
Zulamho <amkhadov2014@mail.ru>
2021-09-21 18:39:46 +0300

    Сделали регистрацию и вход

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-
+import SigninPage from "./pages/SigninPage/SigninPage";
+import SigninUpPage from "./pages/SigninUpPage/SigninUpPage";
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-          
+        <Route path="/signin">
+          <SigninPage />
+        </Route>
+        <Route path="/signinUp">
+          <SigninUpPage />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
 }
+
+export default App;
---
+++ client/src/components/pages/SigninPage/SigninPage.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router";
+import { auth } from "../../../redux/features/application";
+
+function SigninPage() {
+  const dispatch = useDispatch();
+
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const token = useSelector((state) => state.application.token);
+
+  const signingIn = useSelector((state) => state.application.signingIn);
+  const error = useSelector((state) => state.application.error);
+
+  const handleChangeLogin = (e) => {
+    setLogin(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleTransfer = () => {
+    dispatch(auth(login, password));
+  };
+  //const classes = useStyles();
+  // if (token) {
+  //   return <Redirect to="/" />;
+  // }
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={handleChangeLogin}
+        value={login}
+        placeholder="Введите логин"
+      />
+      <br />
+      <input
+        type="password"
+        onChange={handleChangePassword}
+        value={password}
+        type="password"
+        placeholder="Введите пароль"
+      />
+      <br />
+      <button onClick={handleTransfer}>Вход</button>
+    </>
+  );
+}
+
+export default SigninPage;
