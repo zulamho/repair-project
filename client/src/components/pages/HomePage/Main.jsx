605ea9563f6fff498da1e1dd876222949cab820b
Aliev <thebestdevelopering@gmail.com>
2021-09-23 17:45:24 +0300

    дизайн главной страницы-1

--- client/src/components/pages/HomePage/Main.jsx
+++ client/src/components/pages/HomePage/Main.jsx
@@ -1,39 +1,42 @@
-import { Grid,Card, CardMedia } from "@material-ui/core";
+import { Grid, Card, CardMedia } from "@material-ui/core";
 import { React, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
-import { fetchService } from '../../../redux/features/service'
+import { fetchService } from "../../../redux/features/service";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
     img: {
       width: "300px",
-      height: "300px"
-    }}))
-
+      height: "300px",
+    },
+    content: {
+      width: "300px",
+    },
+  })
+);
 
 function Main() {
   const dispatch = useDispatch();
   const classes = useStyles();
   const service = useSelector((state) => state.service.service);
 
-//   useEffect(() => {
-//       dispatch(fetchService())
-//   }, [dispatch])
+  //   useEffect(() => {
+  //       dispatch(fetchService())
+  //   }, [dispatch])
 
   return service?.map((item) => {
-     
     return (
-        <Grid>
-            {item.name}
+      <Grid className={classes.content}>
+        {item.name}
         <Card>
-            <CardMedia
-              className={classes.img}
-              image={`http://localhost:4000${item.pathImages}`}
-            />
-            </Card>
-            </Grid>
+          <CardMedia
+            className={classes.img}
+            image={`http://localhost:4000${item.pathImages}`}
+          />
+        </Card>
+      </Grid>
     );
   });
 }
